0e28d23ae6abb300945abf7fcd7799932a07f4f3
ron881212 <ron881212@gmail.com>
2018-12-20 20:33:51 -0500

    added firebase and completed html

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDK7l5LWJOsCvEl2L5OZYdfXXBBukW45Z4",
+    authDomain: "train-scheduling-6bcbf.firebaseapp.com",
+    databaseURL: "https://train-scheduling-6bcbf.firebaseio.com",
+    projectId: "train-scheduling-6bcbf",
+    storageBucket: "train-scheduling-6bcbf.appspot.com",
+    messagingSenderId: "286048736152"
+};
+  
+firebase.initializeApp(config);
