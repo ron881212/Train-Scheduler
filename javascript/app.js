10904d876152796b01e9358168012f27f36dd1d0
ron881212 <ron881212@gmail.com>
2018-12-20 21:31:42 -0500

    added function to add data to firebase

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,9 @@
+// global variables
+var trainName = "";
+var destination = "";
+var frequency = "";
+var firstTrain = "";
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDK7l5LWJOsCvEl2L5OZYdfXXBBukW45Z4",
@@ -7,5 +13,44 @@ var config = {
     storageBucket: "train-scheduling-6bcbf.appspot.com",
     messagingSenderId: "286048736152"
 };
-  
+
+// iniatializing firebase
 firebase.initializeApp(config);
+
+// storing my database in a variable
+var database = firebase.database();
+
+// submit button function
+$(document).on("click", "#addTrain", function (event) {
+    event.preventDefault();
+
+    // stores the input in variables
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    frequency = $("#frequency").val().trim();
+    firstTrain = $("#firstTrain").val().trim();
+    
+    // making table rows and puting them in variables
+    var tableRow = $("<tr>");
+    var tableData1 = $("<td>");
+    tableData1.text(trainName);
+    var tableData2 = $("<td>");
+    tableData2.text(destination);
+    var tableData3 = $("<td>");
+    tableData3.text(frequency);
+    var tableData4 = $("<td>");
+    tableData4.text(firstTrain);
+    var tableData5 = $("<td>");
+    tableData5.text();
+
+    // pushing and not setting tables in firebase
+    database.ref().push({
+        trainName: trainName,
+        trainDestination: destination,
+        trainLeaving: firstTrain,
+        trainFrequency: frequency,
+        
+        // adds times stamp to entries 
+        trainAddedTimestamp: firebase.database.ServerValue.TIMESTAMP
+    });
+});
