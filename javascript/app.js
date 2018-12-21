60e242a192ab3d135f70295b49540063ccf67253
ron881212 <ron881212@gmail.com>
2018-12-20 22:43:37 -0500

    added form valadition and moment js cdn

--- javascript/app.js
+++ javascript/app.js
@@ -23,13 +23,15 @@ var database = firebase.database();
 // submit button function
 $(document).on("click", "#addTrain", function (event) {
     event.preventDefault();
-
+    $("#warning").html("");
     // stores the input in variables
     trainName = $("#trainName").val().trim();
     destination = $("#destination").val().trim();
     frequency = $("#frequency").val().trim();
     firstTrain = $("#firstTrain").val().trim();
     
+    // a little form valadation
+    if((trainName !== "") && (destination !== "") && (frequency !== "") && (firstTrain !== "")){
     // making table rows and puting them in variables
     var tableRow = $("<tr>");
     var tableData1 = $("<td>");
@@ -42,15 +44,46 @@ $(document).on("click", "#addTrain", function (event) {
     tableData4.text(firstTrain);
     var tableData5 = $("<td>");
     tableData5.text();
-
+    
     // pushing and not setting tables in firebase
     database.ref().push({
         trainName: trainName,
         trainDestination: destination,
         trainLeaving: firstTrain,
         trainFrequency: frequency,
-        
+
         // adds times stamp to entries 
         trainAddedTimestamp: firebase.database.ServerValue.TIMESTAMP
     });
+}   else{ $("#warning").html("<span>Must fill out all forms</span>"); }
+
+});
+
+// function to update site everytime child is added
+database.ref().on("child_added", function (snapshot) {
+    var sv = snapshot.val();
+
+    console.log(sv.trainName);
+    console.log(sv.trainDestination);
+    console.log(sv.trainLeaving);
+    console.log(sv.trainFrequency);
+
+    var tableRow = $("<tr>");
+    var tableData1 = $("<td>");
+    tableData1.text(sv.trainName);
+    var tableData2 = $("<td>");
+    tableData2.text(sv.trainDestination);
+    var tableData3 = $("<td>");
+    tableData3.text(sv.trainLeaving);
+    var tableData4 = $("<td>");
+    tableData4.text(sv.trainFrequency);
+    var tableData5 = $("<td>");
+    tableData5.text();
+
+    tableRow.append(tableData1, tableData2, tableData3, tableData4, tableData5);
+    $(".table").append(tableRow);
+
+}, function (error) {
+    console.log("Errors: " + error);
+
 });
